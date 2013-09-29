e16dc3097519f69b21f2007b2f86ebaa7ea175ce
Euan de Kock <euan@dekock.net>
2013-09-30 02:09:17 +0800

    Added missing juce files

---
+++ JuceLibraryCode/BinaryData.h
@@ -0,0 +1,18 @@
+/* =========================================================================================
+
+   This is an auto-generated file: Any edits you make may be overwritten!
+
+*/
+
+namespace BinaryData
+{
+    extern const char*   klogo128x128_png;
+    const int            klogo128x128_pngSize = 7604;
+
+    extern const char*   klogo32x32_png;
+    const int            klogo32x32_pngSize = 1247;
+
+    // If you provide the name of one of the binary resource variables above, this function will
+    // return the corresponding data and its size (or a null pointer if the name isn't found).
+    const char* getNamedResource (const char* resourceNameUTF8, int& dataSizeInBytes) throw();
+}
